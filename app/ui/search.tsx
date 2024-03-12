ddcb92e445671c4be217b6a9328dc15441b33f33
willywang <wy.ang1991@gmail.com>
2024-03-12 15:15:36 +0800

    feat: auth

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -12,6 +12,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const handleSearch = useDebouncedCallback((term: string) => {
     console.log(term);
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
@@ -31,7 +32,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
-        defaultValue={`${searchParams.get('query')?.toString()}`}
+        defaultValue={`${searchParams.get('query') || ''}`}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
